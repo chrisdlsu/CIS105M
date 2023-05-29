1add1e93a1f8f5b9627ae21fc3db5cd52c11b739
chrisdlsu <chris_perez@dlsu.edu.ph>
2023-05-29 19:21:24 +0800

    refinement of my exercise 01

--- 20230525/Exercise01.java
+++ 20230525/Exercise01.java
@@ -2,23 +2,16 @@ import java.util.Scanner;  // Import the Scanner class
 
 public class Exercise01 {
     public static void main(String[] args) {
-        System.out.println("HALLOOO");
-
-        //sample string
-
-        //String testDate = "03-25-2023";
-        String sDateResponse = "no response yet";
-
-        // create scanner
-        Scanner scAskDate = new Scanner(System.in);
-
-        //Scanner scAskDate;
-
+        //VARIABLES
+        Scanner scAskDate = new Scanner(System.in); // scanner to ask the user
+        String sDateResponse = "no response yet"; // variable to place user response in
 
         //asking input
         System.out.print("Give me a Date in the format of mm-dd-yy or mm-dd-yyyy ");
         sDateResponse = scAskDate.next();
         
+        //TESTS
+        // String testDate = "03-25-2023";
         // String length tester
         /* System.out.println(sDateResponse.length()); */
 
@@ -26,10 +19,11 @@ public class Exercise01 {
         String substrDD = sDateResponse.substring(3, 5);
         String substrYY;
 
+        // Determines with the right substring indices depending on the user's year input format
         if (sDateResponse.length() < 10) {
             substrYY = sDateResponse.substring(6, 8);
         } else {
-            substrYY = sDateResponse.substring(8, 10);
+            substrYY = sDateResponse.substring(6, 10);
         }
 /* 
         String substrMM = testDate.substring(0, 2);
@@ -91,7 +85,16 @@ public class Exercise01 {
 
         //print the date in Mmm d, yyyy format
         //System.out.println(sMonth + " " + nDay + ", " + nYear);
-        System.out.println(MMconvert + " " + substrDD + ", 20" + substrYY);
+        //System.out.println(MMconvert + " " + substrDD + ", 20" + substrYY);
+
+        if (sDateResponse.length() < 10) {
+          //substrYY = sDateResponse.substring(6, 8);
+          System.out.println(MMconvert + " " + substrDD + ", 20" + substrYY);
+        } else {
+          //substrYY = sDateResponse.substring(8, 10);
+          System.out.println(MMconvert + " " + substrDD + ", " + substrYY);
+
+        }
         
         //close scanner
         scAskDate.close();
