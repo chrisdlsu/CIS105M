60d611377c6ea31a4bd73b3e9796e8a75343300e
chrisdlsu <chris_perez@dlsu.edu.ph>
2023-05-29 21:10:26 +0800

    testing making classes

--- 20230525exercises2/Exercise.java
+++ 20230525exercises2/Exercise.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Exercise {
     public static void main(String[] args) {
-        System.out.println("hello");
+        
+        // creating an object out of my Square class
+        SquareObject sqOne = new SquareObject();
+
+        // a scanner to ask input
+        Scanner sc = new Scanner(System.in);
+
+        // original length
+        System.out.println(sqOne.getLength());
+
+        // asking for new one
+        System.out.println("What length of the side would you want it to be?");
+        int scInput;
+        scInput = sc.nextInt();
+
+        sqOne.setLength(scInput);
+        System.out.println("new one is: " + scInput);
+        //System.out.println(sqOne.getLength());
+
+        sc.close();
+
+
     }
 }
