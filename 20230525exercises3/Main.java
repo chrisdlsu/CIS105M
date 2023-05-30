3c39d1bde191029536c3a5acdd05b0faa26bfd80
chrisdlsu <chris_perez@dlsu.edu.ph>
2023-05-30 17:54:43 +0800

    aligning files

---
+++ 20230525exercises3/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        /* Make a pail class and generate an object out of that */
+
+        System.out.println("I am making a pail obecjt!");
+
+    }
+}
---
+++ 20230525exercises3/pail.java
@@ -0,0 +1,18 @@
+public class pail {
+    //ATTRIBUTES
+   private String paleName;
+   private int nCapacity;
+   private Boolean isFull;
+
+    //CONSTRUCTORS
+    
+    //BEHAVIORS
+    //SETTERS
+    public void setPaleName (String p) {
+        this.paleName = "0";
+    }
+
+    //GET
+    //OTHER METHODS
+
+}
